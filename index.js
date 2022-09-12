85274ce9f4ab2e29908c5911dcc797c003bcb4c1
Tejas Suresh Vispute <tejasvispute25@gmail.com>
2022-09-12 12:10:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,177 @@
+// accessing html elements here
+const infobox = document.querySelector(".info-box");
+const Continue = document.querySelector("#continue");
+const categories = document.querySelector(".categories-box");
+const backbutton = document.querySelector(".backbtn button");
+const probab = document.querySelector(".probab");
+const pipe = document.querySelector(".pipe");
+const ages = document.querySelector(".ages");
+const profit_loss = document.querySelector(".profit_loss");
+const QuestionBox = document.querySelector(".Question-box");
+let answer_options = document.querySelector(".answer-options");
+const timecount = document.querySelector("#timer");
+const score = document.querySelector("#score");
+const result = document.querySelector(".result-container");
+let username = document.querySelector(".input-val").value;
+
+
+// if continue button clicked
+Continue.onclick = () => {
+  categories.classList.add("activeinfo"); //for showing options section
+  infobox.classList.add("disp"); //for hiding infobox section
+};
+// for remove activeinfo
+backbutton.onclick = () => {
+  categories.classList.remove("activeinfo");
+  infobox.classList.remove("disp"); //for hiding infobox section
+};
+
+// for printing user input name on screen
+printname = () => {
+  const val = document.querySelector(".input-val").value;
+  document.querySelector("#displayname").innerHTML = "Welcome " + val;
+  document.querySelector(".name").innerHTML = val;
+ 
+};
+
+// for adding question of probabilities
+probab.onclick = () => {
+  categories.classList.remove("activeinfo");
+  QuestionBox.classList.add("activeQuiz"); //for showing question section
+  document.querySelector(".title-bar").innerHTML = "PROBABILITY";
+  showQuestion(0);
+  queCounter(1);
+  startTimer(300);
+  printname();
+};
+
+// pipe.onclick = () => {
+//     categories.classList.remove("activeinfo");
+//     QuestionBox.classList.add("activeQuiz"); //for showing question section
+// };
+// ages.onclick = () => {
+//     categories.classList.remove("activeinfo");
+//     QuestionBox.classList.add("activeQuiz"); //for showing question section
+// };
+// profit_loss.onclick = () => {
+//     categories.classList.remove("activeinfo");
+//     QuestionBox.classList.add("activeQuiz"); //for showing question section
+// };
+
+let qu_count = 0;
+let qu_numb = 1;
+let counter;
+let userscore = 0;
+
+const result_box = document.querySelector(".result-container");
+const restart = document.querySelector(".restart");
+const home = document.querySelector(".home");
+
+// next button accessed here
+const next_button = QuestionBox.querySelector(".btn");
+next_button.onclick = () => {
+  if (qu_count < PROBABILITY.length - 1) {
+    qu_count++;
+    qu_numb++;
+    score.innerHTML = "score: " + userscore;
+    showQuestion(qu_count);
+    queCounter(qu_numb);
+    next_button.classList.remove("show");
+  } else {
+    console.log("questons are completed");
+    showResultBox();
+  }
+};
+
+// accessed questions and print here
+showQuestion = (index) => {
+  const question_text = document.querySelector(".Question");
+
+  let qu_tag =
+    "<span>" +
+    PROBABILITY[index].numb +
+    ". " +
+    PROBABILITY[index].question +
+    "</span>";
+  let option_tag =
+    '<span class="option">' +
+    PROBABILITY[index].option[0] +
+    "</span>" +
+    '<span class="option">' +
+    PROBABILITY[index].option[1] + //add loops here
+    "</span>" +
+    '<span class="option">' +
+    PROBABILITY[index].option[2] +
+    "</span>" +
+    '<span class="option">' +
+    PROBABILITY[index].option[3] +
+    "</span>";
+
+  question_text.innerHTML = qu_tag;
+  answer_options.innerHTML = option_tag;
+  const option = answer_options.querySelectorAll(".option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)"); //adding onclick event on options
+  }
+};
+
+function optionSelected(answer) {
+  let userAns = answer.textContent;
+  let correctAns = PROBABILITY[qu_count].answer;
+  let alloptions = answer_options.children.length;
+  if (userAns == correctAns) {
+    answer.classList.add("correctAns");
+    userscore += 1;
+    console.log("Answer is correct"); //if answer is correct background color of answer will be green
+  } else {
+    answer.classList.add("incorrectAns");
+    console.log("Answer is wrong"); // if answer is wrong background color will be red.
+
+    for (let i = 0; i < alloptions; i++) {
+      if (answer_options.children[i].textContent == correctAns) {
+        answer_options.children[i].classList.add("correctAns");
+        console.log("working");
+      }
+    }
+  }
+
+  // once user click one option disable all other options
+  for (let i = 0; i < alloptions; i++) {
+    answer_options.children[i].classList.add("disabled");
+  }
+
+  next_button.classList.add("show");
+}
+
+// Timer of 300 seconds
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timecount.textContent = time;
+    time--;
+  }
+}
+
+// top question count increment after every next button clicked
+function queCounter(index) {
+  const top_question_count = QuestionBox.querySelector(".qucount");
+  let totalQuestioncount =
+    "<span>" + index + "/" + PROBABILITY.length + "</span>";
+  top_question_count.innerHTML = totalQuestioncount;
+}
+
+// Show Result Box
+
+function showResultBox() {
+  categories.classList.remove("activeinfo");
+  QuestionBox.classList.remove("activeQuiz");
+  result.classList.add("activeResult");
+  const name = document.querySelector(".name-result");
+  const timetaken = document.querySelector(".time_taken");
+  const attempts = document.querySelector(".attempts");
+  const correct = document.querySelector(".correct");
+  const wrong = document.querySelector(".wrong");
+  const Percentage = document.querySelector(".Percentage");
+  nameTag= '<span>'+username +"your result is" +'</span>'
+  name.innerHTML=nameTag;
+}
---
+++ questions.js
@@ -0,0 +1,491 @@
+const PROBABILITY = [
+  {
+    numb: 1,
+    question:
+      "Tickets numbered 1 to 20 are mixed up and then a ticket is drawn at random. What is the probability that the ticket drawn has a number which is a multiple of 3 or 5?",
+    answer: "9/20",
+    option: [
+      "1/2",
+      "2/5",
+      "8/15",
+      "9/20"
+    ]
+
+  },
+
+  {
+    numb: 2,
+    question:
+      "What is the probability of getting a sum 9 from two throws of a dice?",
+    answer: "1/9",
+    option: [
+      "3/9",
+      "2/9",
+      "1/9",
+      "5/12"
+    ]
+
+  },
+  {
+    numb: 3,
+    question:
+      "Three unbiased coins are tossed. What is the probability of getting at most two heads?",
+    answer: "7/18",
+    option: [
+      "5/15",
+      "6/10",
+      "2/30",
+      "7/18"
+    ]
+
+  },
+  {
+    numb: 4,
+    question:
+      "A card is drawn from a pack of 52 cards. The probability of getting a queen of club or a king of heart is:",
+    answer: "1/26",
+    option: [
+      "1/26",
+      "4/10",
+      "3/29",
+      "8/23"
+    ]
+
+  },
+  {
+    numb: 5,
+    question:
+      "A bag contains 4 white, 5 red and 6 blue balls. Three balls are drawn at random from the bag. The probability that all of them are red, is:",
+    answer: "2/91",
+    option: [
+      "7/20",
+      "4/21",
+      "2/87",
+      "2/91"
+    ]
+
+  },
+  {
+    numb: 6,
+    question:
+      "In a lottery, there are 10 prizes and 25 blanks. A lottery is drawn at random. What is the probability of getting a prize?",
+    answer: "5/7",
+    option: [
+      "5/7",
+      "5/9",
+      "2/9",
+      "2/6"
+    ]
+
+  },
+  {
+    numb: 7,
+    question:
+      "Two cards are drawn together from a pack of 52 cards. The probability that one is a spade and one is a heart, is:",
+    answer: "13/102",
+    option: [
+      "20/87",
+      "13/102",
+      "26/56",
+      "12,54"
+    ]
+
+  },
+  {
+    numb: 8,
+    question:
+      "One card is drawn at random from a pack of 52 cards. What is the probability that the card drawn is a face card (Jack, Queen and King only)?",
+    answer: "3/13",
+    option: [
+      "3/13",
+      "6/20",
+      "10/13",
+      "9/12",
+    ]
+
+  },
+  {
+    numb: 9,
+    question:
+      "Two dice are tossed. The probability that the total score is a prime number is:",
+    answer: "5/12",
+    option: [
+      "2/12",
+      "5/12",
+      "9/16",
+      "13/24",
+    ]
+
+  },
+  {
+    numb: 10,
+    question:
+      "Out of 17 applicants, there are 8 boys and 9 girls. Two persons are to be selected for the job. Find the probability that at least one of the selected persons will be a girl.",
+    answer: "27/34",
+    option: [
+      "27/34",
+      "13/24",
+      "10/28",
+      "9/25",
+    ]
+
+  },
+];
+const PIPES_AND_CISTERNS = [
+  {
+    numb: 1,
+    question:
+      "Three pipes A, B and C can fill a tank from empty to full in 30 minutes, 20 minutes, and 10 minutes respectively. When the tank is empty, all the three pipes are opened. A, B and C discharge chemical solutions P,Q and R respectively. What is the proportion of the solution R in the liquid in the tank after 3 minutes?",
+    answer: "6/11",
+    options: [
+      "5/11",
+      "6/11",
+      "7/11",
+      "8/11",
+    ]
+  },
+  {
+    numb: 2,
+    question:
+      "A tank is filled by three pipes with uniform flow. The first two pipes operating simultaneously fill the tank in the same time during which the tank is filled by the third pipe alone. The second pipe fills the tank 5 hours faster than the first pipe and 4 hours slower than the third pipe. The time required by the first pipe is:",
+    answer: "15 hours",
+    options: [
+      "6 hours",
+      "10 hours",
+      "15 hours",
+      "20 hours",
+    ]
+
+  },
+  {
+    numb: 3,
+    question:
+      "Two pipes A and B can fill a cistern in 37 minutes and 45 minutes respectively. Both pipes are opened. The cistern will be filled in just half an hour, if the B is turned off after:",
+    answer: "9 mins",
+    options: [
+      "5 mins",
+      "9 mins",
+      "10 mins",
+      "15 mins",
+    ]
+
+  },
+  {
+    numb: 4,
+    question:
+      "Two pipes can fill a tank in 20 and 24 minutes respectively and a waste pipe can empty 3 gallons per minute. All the three pipes working together can fill the tank in 15 minutes. The capacity of the tank is:",
+    answer: "120 gallons",
+    options: [
+      "60 gallons",
+      "100 gallons",
+      "120 gallons",
+      "180 gallons",
+    ]
+
+  },
+  {
+    numb: 5,
+    question:
+      "A tank is filled in 5 hours by three pipes A, B and C. The pipe C is twice as fast as B and B is twice as fast as A. How much time will pipe A alone take to fill the tank?",
+    answer: "35 hours",
+    options: [
+      "20 hours",
+      "25 hours",
+      "35 hours",
+      "Cannot be determined",
+    ]
+
+  },
+  {
+    numb: 6,
+    question:
+      "Two pipes A and B together can fill a cistern in 4 hours. Had they been opened separately, then B would have taken 6 hours more than A to fill the cistern. How much time will be taken by A to fill the cistern separately?",
+    answer: "6 hours",
+    options: [
+      "1 hours",
+      "2 hours",
+      "6 hours",
+      "8 hours",
+    ]
+  },
+  {
+    numb: 7,
+    question:
+      "Two pipes A and B can fill a tank in 20 and 30 minutes respectively. If both the pipes are used together, then how long will it take to fill the tank?",
+    answer: "12 mins",
+    options: [
+      "12  mins",
+      "15  mins",
+      "25  mins",
+      "30  mins",
+    ]
+
+  },
+  {
+    numb: 8,
+    question:
+      "Three pipes A, B and C can fill a tank in 6 hours. After working at it together for 2 hours, C is closed and A and B can fill the remaining part in 7 hours. The number of hours taken by C alone to fill the tank is:",
+    answer: "14",
+    options: [
+      "10",
+      "12",
+      "14",
+      "16",
+    ]
+
+
+  },
+  {
+    numb: 9,
+    question:
+      "Two pipes A and B can fill a tank in 15 minutes and 20 minutes respectively. Both the pipes are opened together but after 4 minutes, pipe A is turned off. What is the total time required to fill the tank?",
+    answer: "14min 40sec",
+    options: [
+      "10min 20sec",
+      "11min 45sec",
+      "12min 30 sec",
+      "14min 40sec",
+    ]
+
+  },
+  {
+    numb: 10,
+    question:
+      "One pipe can fill a tank three times as fast as another pipe. If together the two pipes can fill the tank in 36 minutes, then the slower pipe alone will be able to fill the tank in:",
+    answer: "144",
+    options: [
+      "81",
+      "108",
+      "144",
+      "192",
+    ]
+
+  },
+];
+const PROBLEMS_ON_AGES = [
+  {
+    question:
+      "The sum of ages of 5 children born at the intervals of 3 years each is 50 years. What is the age of the youngest child?",
+    options: {
+      a: "4 years",
+      b: "8 years",
+      c: "10 years",
+      d: "None",
+    },
+    answer: "4 years",
+  },
+  {
+    question:
+      "A is two years older than B who is twice as old as C. If the total of the ages of A, B and C be 27, then how old is B?",
+    options: {
+      a: "7",
+      b: "8",
+      c: "9",
+      d: "10",
+    },
+    answer: "10",
+  },
+  {
+    question:
+      "Present ages of Sameer and Anand are in the ratio of 5 : 4 respectively. Three years hence, the ratio of their ages will become 11 : 9 respectively. What is Anand's present age in years?",
+    options: {
+      a: "24",
+      b: "27",
+      c: "40",
+      d: "None",
+    },
+    answer: "24",
+  },
+  {
+    question:
+      "A man is 24 years older than his son. In two years, his age will be twice the age of his son. The present age of his son is:",
+    options: {
+      a: "14",
+      b: "18",
+      c: "20",
+      d: "22",
+    },
+    answer: "22",
+  },
+  {
+    question:
+      "Six years ago, the ratio of the ages of Kunal and Sagar was 6 : 5. Four years hence, the ratio of their ages will be 11 : 10. What is Sagar's age at present?",
+    options: {
+      a: "16",
+      b: "18",
+      c: "20",
+      d: "26",
+    },
+    answer: "16",
+  },
+  {
+    question:
+      "The sum of the present ages of a father and his son is 60 years. Six years ago, father's age was five times the age of the son. After 6 years, son's age will be:",
+    options: {
+      a: "19",
+      b: "22",
+      c: "21",
+      d: "20",
+    },
+    answer: "20",
+  },
+  {
+    question:
+      "At present, the ratio between the ages of Arun and Deepak is 4 : 3. After 6 years, Arun's age will be 26 years. What is the age of Deepak at present ?",
+    options: {
+      a: "15",
+      b: "18",
+      c: "19",
+      d: "12",
+    },
+    answer: "15",
+  },
+  {
+    question:
+      "Sachin is younger than Rahul by 7 years. If their ages are in the respective ratio of 7 : 9, how old is Sachin?",
+    options: {
+      a: "24.5",
+      b: "28",
+      c: "22.5",
+      d: "19",
+    },
+    answer: "24.5",
+  },
+  {
+    question:
+      "Ayesha's father was 38 years of age when she was born while her mother was 36 years old when her brother four years younger to her was born. What is the difference between the ages of her parents?",
+    options: {
+      a: "2",
+      b: "3",
+      c: "6",
+      d: "10",
+    },
+    answer: "6",
+  },
+  {
+    question:
+      "A person's present age is two-fifth of the age of his mother. After 8 years, he will be one-half of the age of his mother. How old is the mother at present?",
+    options: {
+      a: "32",
+      b: "36",
+      c: "40",
+      d: "49",
+    },
+    answer: "40",
+  },
+];
+const PROFITS_AND_LOSSES = [
+  {
+    question:
+      "The cost price of 20 articles is the same as the selling price of x articles. If the profit is 25%, then the value of x is:",
+    options: {
+      a: "15",
+      b: "16",
+      c: "18",
+      d: "25",
+    },
+    answer: "16",
+  },
+  {
+    question:
+      "A vendor bought toffees at 6 for a rupee. How many for a rupee must he sell to gain 20%?",
+    options: {
+      a: "2",
+      b: "6",
+      c: "5",
+      d: "8",
+    },
+    answer: "5",
+  },
+  {
+    question:
+      "Sam purchased 20 dozens of toys at the rate of Rs. 375 per dozen. He sold each one of them at the rate of Rs. 33. What was his percentage profit?",
+    options: {
+      a: "2",
+      b: "5.5",
+      c: "4.5",
+      d: "5.6",
+    },
+    answer: "5.6",
+  },
+  {
+    question:
+      "On selling 17 balls at Rs. 720, there is a loss equal to the cost price of 5 balls. The cost price of a ball is:",
+    options: {
+      a: "25",
+      b: "60",
+      c: "55",
+      d: "40",
+    },
+    answer: "60",
+  },
+  {
+    question:
+      "By selling an article at Rs. 800, a shopkeeper makes a profit of 25%. At what price should he sell the article so as to make a loss of 25%?",
+    options: {
+      a: "220",
+      b: "480",
+      c: "300",
+      d: "460",
+    },
+    answer: "480",
+  },
+  {
+    question:
+      "Profit after selling a commodity for Rs. 425 is same as loss after selling it for Rs.355. The cost of the commodity is:",
+    options: {
+      a: "120",
+      b: "200",
+      c: "390",
+      d: "480",
+    },
+    answer: "390",
+  },
+  {
+    question:
+      "Bhajan Singh purchased 120 reams of paper at Rs. 80 per ream. He spent Rs. 280 on transportation. Paid octrai at the rate of 40 paise per ream and paid Rs. 72 to the coolie. If he wants to have a gain of 8 %. What must be the selling price per ream?",
+    options: {
+      a: "76",
+      b: "23",
+      c: "87",
+      d: "90",
+    },
+    answer: "90",
+  },
+  {
+    question:
+      "A person sold a pen at Rs. 96 in such a way that his percentage profit is same as the cost price of the watch. If he sells it at twice the percentage profit of its previous percentage profit then new selling price will be?",
+    options: {
+      a: "100",
+      b: "220",
+      c: "132",
+      d: "126",
+    },
+    answer: "132",
+  },
+  {
+    question:
+      " The cost price of a radio is Rs.1500 and it was sold for Rs.1230, find the loss %?",
+    options: {
+      a: "9%",
+      b: "19%",
+      c: "23%",
+      d: "18%",
+    },
+    answer: "18%",
+  },
+  {
+    question:
+      "A watch was sold at a loss of 10%. If it was sold for Rs.140 more, there would have been a gain of 4%. What is the cost price?",
+    options: {
+      a: "1000",
+      b: "320",
+      c: "780",
+      d: "820",
+    },
+    answer: "1000",
+  },
+];
+
+
+
